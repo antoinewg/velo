ce8a5783cce483d34794547325cdddfad29dfa9a
AntoineG <antoine.garcia@polyconseil.fr>
2020-05-07 21:59:21 +0200

    add basic visualisation

---
+++ src/api/velib.py
@@ -0,0 +1,46 @@
+import requests as re
+import pandas as pd
+
+
+BASE_VELIB_URL = "https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/"
+STATION_STATUS_URL = BASE_VELIB_URL + "station_status.json"
+STATION_INFORMATION_URL = BASE_VELIB_URL + "station_information.json"
+
+
+def get_station_info():
+    information = re.get(STATION_INFORMATION_URL).json()
+    records = information.get("data", {}).get("stations", [])
+    return pd.DataFrame.from_records(
+        [
+            {
+                "station_id": record["station_id"],
+                "name": record["name"],
+                "lat": record["lat"],
+                "lon": record["lon"],
+                "capacity": record["capacity"],
+            }
+            for record in records
+        ]
+    )
+
+
+def get_station_statuses():
+    statuses = re.get(STATION_STATUS_URL).json()
+    records = statuses.get("data", {}).get("stations", [])
+    return pd.DataFrame.from_records(
+        [
+            {
+                "station_id": record["station_id"],
+                "num_bikes_available": record["num_bikes_available"],
+                # "num_bikes_available_mechanical": record["num_bikes_available_types"][
+                #     "mechanical"
+                # ],
+                # "num_bikes_available_ebike": record["num_bikes_available_types"][
+                #     "ebike"
+                # ],
+                # "num_docks_available": record["num_docks_available"],
+                "last_reported": record["last_reported"],
+            }
+            for record in records
+        ]
+    )
--- src/app.py
+++ src/app.py
@@ -1,3 +1,62 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import pydeck as pdk
+
+from api.velib import get_station_info, get_station_statuses
+
+
+st.title("Velo 🚴🏻‍♂️")
+st.write("Check the availabity of docked bike in your city !")
+st.markdown("_(if your city is Paris)_")
+
+
+@st.cache
+def get_live_station_information():
+    info = get_station_info()
+    statuses = get_station_statuses()
+    return info.merge(statuses)
+
+
+show_capacity = st.sidebar.checkbox("Show capacity", True)
+show_availability = st.sidebar.checkbox("Show availability", False)
+show_non_availability = st.sidebar.checkbox("Show non-availability", False)
+
+map_data = get_live_station_information()
+st.pydeck_chart(
+    pdk.Deck(
+        map_style="mapbox://styles/mapbox/dark-v9",
+        initial_view_state=pdk.ViewState(
+            latitude=map_data["lat"].mean(),
+            longitude=map_data["lon"].mean(),
+            zoom=11,
+            pitch=20,
+        ),
+        layers=[
+            show_capacity
+            and pdk.Layer(
+                "HeatmapLayer",
+                data=map_data,
+                opacity=0.1,
+                get_position="[lon, lat]",
+                get_weight="capacity",
+            ),
+            show_availability
+            and pdk.Layer(
+                "HeatmapLayer",
+                data=map_data,
+                opacity=0.1,
+                get_position="[lon, lat]",
+                get_weight="num_bikes_available / capacity",
+            ),
+            show_non_availability
+            and pdk.Layer(
+                "HeatmapLayer",
+                data=map_data,
+                opacity=0.1,
+                get_position="[lon, lat]",
+                get_weight="1 - num_bikes_available / capacity",
+            ),
+        ],
+    )
+)
